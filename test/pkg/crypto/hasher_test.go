db109ea6be2db4ff2636db351e611999b88789e8
Bruno Lombardi <djfrizer1@gmail.com>
2023-03-30 16:51:35 -0300

    test: hasher test

---
+++ test/pkg/crypto/hasher_test.go
@@ -0,0 +1,41 @@
+package crypto_tests
+
+import (
+	"testing"
+	"vanir/internal/pkg/crypto"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type BCryptHasherSuite struct {
+	suite.Suite
+	hasher *crypto.BCryptHasher
+}
+
+func (sut *BCryptHasherSuite) BeforeTest(_, _ string) {
+	sut.hasher = &crypto.BCryptHasher{}
+}
+
+func (sut *BCryptHasherSuite) TestSmokeGetHasher() {
+	hasher, ok := crypto.GetHasher().(*crypto.BCryptHasher)
+	sut.NotNil(hasher)
+	sut.True(ok)
+}
+
+func (sut *BCryptHasherSuite) TestShouldReturnAHashedString() {
+	plain := "a_string_to_hash"
+	hashed := sut.hasher.HashAndSalt([]byte(plain))
+	sut.NotEmpty(hashed)
+	sut.NotEqual(plain, hashed)
+}
+
+func (sut *BCryptHasherSuite) TestShouldReturnTrueWhenComparingHashes() {
+	plain := "a_string_to_hash"
+	hashed := sut.hasher.HashAndSalt([]byte(plain))
+	isCompareSuccessful := sut.hasher.CompareHashes(hashed, []byte(plain))
+	sut.True(isCompareSuccessful)
+}
+
+func TestBCryptHasherSuite(t *testing.T) {
+	suite.Run(t, &BCryptHasherSuite{})
+}
